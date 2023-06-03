e9e0afbabff9a4f3f32aa441bf02ae896d055113
Peter Medbury <peter@Deep-Thought.local>
2023-06-03 19:44:00 +1000

    Split Character Sets and User Options into 2 separate objects

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,4 +14,5 @@
 1. Created folder structure and copied supplied code (index.html, style.css, script.js) to appropriate locations
 2. Added supplied password character sets (upperCase, lowerCase, number and specials) to js
 3. Added code to prompt for Password Length & test length is valid
-4. Created PasswordSettings object which includes possible characters sets, characters used & length of password  
+4. Created passwordSettings object which includes possible characters sets, characters used & length of password
+5. Split Character Sets and User Options into 2 separate objects - while related characters are pre-defined but users coices are runtime
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -13,17 +13,15 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
-  //Define Password Settings, Choises & Length
-  const passwordSettings = {
+  //Define Object to hold possible character sets
+  const pwdCharacterSets = {
     lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",],
     upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",],
     numeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    special: ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",],
-    passwordLength: 8,
-    useLowerCase: false,
-    useUpperCase: false,
-    usenumeric: false,
-    useSpecial: false
+    special: ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",]
+  }
+  const pwdOptions = {
+    options: { passwordLength: 8, useLowerCase: false, useUpperCase: false, useNumeric: false, useSpecial: false }
   }
 
   // Get password length
@@ -31,39 +29,39 @@ function generatePassword() {
   // Check for numeric
   // Check if >=8 and <=128
 
-  window.alert(passwordSettings["lowerCase"][1]);
+  window.alert(pwdCharacterSets["lowerCase"][1]);
   //loop until user enters a number >=8 and <=128 or cancels 
   do {
-    passwordSettings.passwordLength = window.prompt(
+    pwdOptions.passwordLength = window.prompt(
       "Enter Password Length.\nPasswords must have at least 8 characters and no more than 128 characters"
     );
 
     // if user clicks on Cancel or Clicks OK without entering the number of character
     // end processing and display message 
-    if (!passwordSettings.passwordLength || passwordSettings.passwordLength === undefined) {
+    if (!pwdOptions.passwordLength || pwdOptions.passwordLength === undefined) {
       return "User Cancelled Process";
     }
 
     // Loop until user enters a number >= 8 and <= 128
   } while (
-    Number(passwordSettings.passwordLength) < 8 ||       // test for number NOT >= 8
-    Number(passwordSettings.passwordLength) > 128 ||     // test for number NOT <= 128
-    isNaN(passwordSettings.passwordLength)               // test for entry that is NOT a number
+    Number(pwdOptions.passwordLength) < 8 ||       // test for number NOT >= 8
+    Number(pwdOptions.passwordLength) > 128 ||     // test for number NOT <= 128
+    isNaN(pwdOptions.passwordLength)               // test for entry that is NOT a number
   );
 
-  passwordSettings.useLowerCase = window.prompt(
+  pwdOptions.useLowerCase = window.prompt(
     "Include lowercase Characters in your password? Choose Y or N"
   );
-  passwordSettings.useUpperCase = window.prompt(
+  pwdOptions.useUpperCase = window.prompt(
     "Include lowercase Characters in your password? Choose Y or N"
   );
-  passwordSettings.useNumeric = window.prompt(
+  pwdOptions.useNumeric = window.prompt(
     "Include lowercase Characters in your password? Choose Y or N"
   );
-  passwordSettings.useSpecial = window.prompt(
+  pwdOptions.useSpecial = window.prompt(
     "Include lowercase Characters in your password? Choose Y or N"
   );
 
   // return the result to writePassword()
-  return passwordSettings.passwordLength + "|" + passwordSettings.useLowerCase + "|" + passwordSettings.useUpperCase + "|" + passwordSettings.useNumeric + "|" + passwordSettings.useSpecial;
+  return pwdOptions.passwordLength + "|" + pwdOptions.useLowerCase + "|" + pwdOptions.useUpperCase + "|" + pwdOptions.useNumeric + "|" + pwdOptions.useSpecial;
 }
